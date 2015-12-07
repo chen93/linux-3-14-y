0810637b0d002042795f1eb6232b4e6eac04ca80
donglinx <donglinx.peng@intel.com>
2015-12-07 14:16:25 +0800

    Add file scripts, one for compile the image file used by qemu-system-arm, the other is for running qemu-system-arm.

---
+++ vexpress_a9_qemu.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+make distclean
+
+make CROSS_COMPILE=arm-linux-gnueabi- ARCH=arm O=./out_vexpress_3_14  vexpress_defconfig
+
+make CROSS_COMPILE=arm-linux-gnueabi- ARCH=arm O=./out_vexpress_3_14  zImage -j2
+
+make CROSS_COMPILE=arm-linux-gnueabi- ARCH=arm O=./out_vexpress_3_14 LOADADDR=0x60003000  uImage -j2
